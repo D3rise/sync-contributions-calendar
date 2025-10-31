aa2386ce530193504c752469eadb13635d8c467e
Vladimir Andrienko <derise2000@gmail.com>
2025-10-31 15:41:41 +0300

    Update gitlab-calendar.js

--- src/gitlab-calendar.js
+++ src/gitlab-calendar.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
-module.exports.getGitlabCalendar = async function getGitlabCalendar (gitlabUsername) {
-  return fetch(`https://gitlab.com/users/${gitlabUsername}/calendar.json`, {
+module.exports.getGitlabCalendar = async function getGitlabCalendar (gitlabUrl, gitlabUsername) {
+  return fetch(`${gitlabUrl}/users/${gitlabUsername}/calendar.json`, {
     headers: {
       accept: 'application/json, text/plain, */*',
       'accept-language': 'en,es;q=0.9,pt;q=0.8'
